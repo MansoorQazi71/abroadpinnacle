a5d085ffc0b4669b47fa4baa35a5f80e3b498bae
MansoorAhmadQazi <mansoorqazi71@gmail.com>
2025-02-23 22:19:21 +0500

    mailing route added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactFormMail;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+Route::post('/send-contact-email', function (Request $request) {
+    $request->validate([
+        'name'    => 'required|max:255',
+        'email'   => 'required|email',
+        'subject' => 'required|max:255',
+    ]);
+
+    $data = [
+        'name'    => $request->input('name'),
+        'email'   => $request->input('email'),
+        'subject' => $request->input('subject'),
+    ];
+
+    Mail::to('abroadpinnacle3@gmail.com')->send(new ContactFormMail($data));
+
+
+    return redirect()->back()->with(['message' => 'Email sent successfully!']);
+})->name('send.contact.email');
